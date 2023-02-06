aa243d39cc835cf65d5aec699810e4580b04f05e
elira saraqini <elira.saraqini@ubt-uni.net>
2023-02-07 00:05:01 +0100

    utils

---
+++ AttendanceTracker/AttendanceTracker.Utils/HashedPassword.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace AttendanceTracker.Utils;
+
+
+public class HashedPassword
+{
+    public HashedPassword(string password, string salt)
+    {
+        Password = password;
+        Salt = salt;
+    }
+
+    public string Password { get; }
+
+    public string Salt { get; }
+}
+
---
+++ AttendanceTracker/AttendanceTracker.Utils/PasswordHasher.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System.Security.Cryptography;
+
+namespace AttendanceTracker.Utils;
+
+
+public static class PasswordHasher
+{
+    public static HashedPassword HashPassword(string pwd)
+    {
+        var saltBytes = RandomNumberGenerator.GetBytes(16);
+        var hashBytes = KeyDerivation.Pbkdf2(
+                            password: pwd,
+                            salt: saltBytes,
+                            prf: KeyDerivationPrf.HMACSHA256,
+                            iterationCount: 100000,
+                            numBytesRequested: 32);
+
+        var salt = Convert.ToBase64String(saltBytes);
+        var hashedPassword = Convert.ToBase64String(hashBytes);
+
+        return new HashedPassword(hashedPassword, salt);
+    }
+
+
+    public static bool ComparePasswords(string plain, string salt, string hashed)
+    {
+        var saltBytes = Convert.FromBase64String(salt);
+        var hashBytes = KeyDerivation.Pbkdf2(
+                            password: plain,
+                            salt: saltBytes,
+                            prf: KeyDerivationPrf.HMACSHA256,
+                            iterationCount: 100000,
+                            numBytesRequested: 32);
+
+        var hashedPassword = Convert.ToBase64String(hashBytes);
+
+        return hashedPassword == hashed;
+    }
+}
